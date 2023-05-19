77bf0196f2ef62e54ef93ee15062afe59d4b98fe
BatsAppDev <ad.stokes.25@gmail.com>
2023-05-19 22:59:07 +0200

    changed name of owner from adamizer25 to stokes-adam

--- infra-core/build/Program.cs
+++ infra-core/build/Program.cs
@@ -31,12 +31,12 @@ Target(BuildContainer, () =>
   .UseWorkDir("/app")
   .Build();
     
- Run("docker", $"tag {ContainerName}:latest ghcr.io/adamizer25/care-calendar/{ContainerName}:{imageTag}");
+ Run("docker", $"tag {ContainerName}:latest ghcr.io/stokes-adam/care-calendar/{ContainerName}:{imageTag}");
 });
 
 Target(PushContainer, () =>
 {
- Run("docker", $"push ghcr.io/adamizer25/care-calendar/{ContainerName}:{imageTag}");
+ Run("docker", $"push ghcr.io/stokes-adam/care-calendar/{ContainerName}:{imageTag}");
 });
 
 Target("default", DependsOn(Clean, Publish, BuildContainer));
