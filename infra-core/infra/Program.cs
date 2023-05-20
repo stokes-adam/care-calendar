993ad7dfabc9adbcc1bbb3474caee0ce18d9b3a8
BatsAppDev <ad.stokes.25@gmail.com>
2023-05-20 11:33:21 +0200

    care calendar pulumi

--- infra-core/infra/Program.cs
+++ infra-core/infra/Program.cs
@@ -1,34 +1,15 @@
 ﻿using Pulumi;
-using Pulumi.Aws;
 using Pulumi.Aws.S3;
+using System.Collections.Generic;
 
 return await Deployment.RunAsync(() =>
 {
-    const string domain = "carecalendar.xyz";
-    const string bookingSubdomain = "booking.carecalendar.xyz"; // this is where the booking app will be hosted
-    const string apiSubdomain = "api.carecalendar.xyz"; // this is where the api will be hosted
+   // Create an AWS resource (S3 Bucket)
+   var bucket = new Bucket("my-bucket");
 
-    var provider = new Provider("care-calendar-provider", new ProviderArgs
-    {
-        Region = "us-east-1",
-    });
-    
-    var options = new CustomResourceOptions
-    {
-        Provider = provider
-    };
-    
-    var bucket = new Bucket("care-calendar-bucket", new BucketArgs
-    {
-    }, options);
-    
-    // hosted zone
-    // s3 bucket
-    // cloudfront distribution
-    // certificate
-    // route53 record
-
-    // rds instance
-    // ecs
-
+   // Export the name of the bucket
+   return new Dictionary<string, object?>
+   {
+      ["bucketName"] = bucket.Id
+   };
 });
