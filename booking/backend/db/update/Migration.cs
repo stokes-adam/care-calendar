252ac8eaa026fbcd9db55d28059c29d9c6778453
Adam Stokes <adamstokes@duck.com>
2023-07-10 12:54:58 +0200

    Yeet migration container. Use lambda invocation instead.

---
+++ booking/backend/db/update/Migration.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using Amazon.Lambda.Core;
+using DbUp;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace update;
+
+public class Migration
+{
+    private readonly string _connectionString =
+        Environment.GetEnvironmentVariable("ConnectionString") ?? "CONNECTION_STRING_NOT_SET";
+
+    public void FunctionHandler()
+    {
+        try
+        {
+            var upgrader =
+                DeployChanges.To
+                    .PostgresqlDatabase(_connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .WithVariablesDisabled()
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                throw result.Error;
+            }
+
+        }
+        catch (Exception e)
+        {
+            // TODO: Log error
+        }
+    }
+}
---
+++ booking/backend/infra/MigrationInfra.cs
@@ -0,0 +1,50 @@
+using Pulumi;
+using Pulumi.Aws.Iam;
+using Pulumi.Aws.Lambda;
+using Pulumi.Aws.Lambda.Inputs;
+
+namespace infra;
+
+public class MigrationInfra
+{
+   public MigrationInfra(DbInfra dbInfra, NetworkInfra networkInfra, CustomResourceOptions customResourceOptions)
+   {
+      var migrationRole = new Role("migrationRole", new RoleArgs
+      {
+         AssumeRolePolicy = @"{
+            ""Version"": ""2012-10-17"",
+            ""Statement"": [
+                {
+                ""Action"": ""sts:AssumeRole"",
+                ""Principal"": {
+                    ""Service"": ""lambda.amazonaws.com""
+                },
+                ""Effect"": ""Allow"",
+                ""Sid"": """"
+                }
+            ]
+            }" 
+      }, customResourceOptions);
+
+      var migrationLambda = new Function("migrationLambda", new FunctionArgs
+      {
+          Code = new FileArchive("./update"),
+          Role = migrationRole.Arn,
+          Handler = "update::update.Migration::FunctionHandler",
+          Runtime = Runtime.Dotnet6,
+          VpcConfig =  new FunctionVpcConfigArgs
+          {
+              VpcId = networkInfra.VpcId,
+              SubnetIds = { networkInfra.Subnet1Id, networkInfra.Subnet2Id },
+              SecurityGroupIds = { networkInfra.SecurityGroupId }
+          },
+          Environment = new FunctionEnvironmentArgs
+          {
+              Variables =
+              {
+                  { "ConnectionString", dbInfra.ConnectionString },
+              }
+          },
+      }, customResourceOptions);
+   }
+}
