7a4361aa80a3955fb794104fe02576c6119edb35
BatsAppDev <ad.stokes.25@gmail.com>
2023-07-19 21:43:35 +0200

    invocation dependson migrationlambda

--- booking/backend/infra/MigrationInfra.cs
+++ booking/backend/infra/MigrationInfra.cs
@@ -8,11 +8,13 @@ namespace infra;
 
 public class MigrationInfra
 {
-   public MigrationInfra(DbInfra dbInfra, NetworkInfra networkInfra, CustomResourceOptions customResourceOptions)
-   {
-      var migrationRole = new Role("migrationRole", new RoleArgs
-      {
-         AssumeRolePolicy = @"{
+    public Invocation MigrationInvocation { get; }
+
+    public MigrationInfra(DbInfra dbInfra, NetworkInfra networkInfra, CustomResourceOptions customResourceOptions)
+    {
+        var migrationRole = new Role("migrationRole", new RoleArgs
+        {
+            AssumeRolePolicy = @"{
             ""Version"": ""2012-10-17"",
             ""Statement"": [
                 {
@@ -24,44 +26,45 @@ public class MigrationInfra
                 ""Sid"": """"
                 }
             ]
-            }" 
-      }, customResourceOptions);
-      
-      var migrationRolePolicyAttachment = new RolePolicyAttachment("migrationRolePolicyAttachment", new RolePolicyAttachmentArgs
-      {
-          Role = migrationRole.Name,
-          PolicyArn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-      }, customResourceOptions);
-      
-      var migrationLambda = new Function("migrationLambda", new FunctionArgs
-      {
-          Code = new FileArchive("./update"),
-          Role = migrationRole.Arn,
-          Handler = "update::update.Migration::FunctionHandler",
-          Runtime = Runtime.Dotnet6,
-          Timeout = 60,
-          VpcConfig =  new FunctionVpcConfigArgs
-          {
-              SubnetIds = { networkInfra.Subnet1Id, networkInfra.Subnet2Id },
-              SecurityGroupIds = { networkInfra.SecurityGroupId }
-          },
-          Environment = new FunctionEnvironmentArgs
-          {
-              Variables =
-              {
-                  { "ConnectionString", dbInfra.ConnectionString },
-              }
-          },
-      }, customResourceOptions);
+            }"
+        }, customResourceOptions);
 
-      var invocation = new Invocation("migration", new()
-      {
+        var migrationRolePolicyAttachment = new RolePolicyAttachment("migrationRolePolicyAttachment",
+            new RolePolicyAttachmentArgs
+            {
+                Role = migrationRole.Name,
+                PolicyArn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+            }, customResourceOptions);
+
+        var migrationLambda = new Function("migrationLambda", new FunctionArgs
+        {
+            Code = new FileArchive("./update"),
+            Role = migrationRole.Arn,
+            Handler = "update::update.Migration::FunctionHandler",
+            Runtime = Runtime.Dotnet6,
+            Timeout = 60,
+            VpcConfig = new FunctionVpcConfigArgs
+            {
+                SubnetIds = { networkInfra.Subnet1Id, networkInfra.Subnet2Id },
+                SecurityGroupIds = { networkInfra.SecurityGroupId }
+            },
+            Environment = new FunctionEnvironmentArgs
+            {
+                Variables =
+                {
+                    { "ConnectionString", dbInfra.ConnectionString },
+                }
+            },
+        }, customResourceOptions);
+
+        MigrationInvocation = new Invocation("migration", new()
+        {
             FunctionName = migrationLambda.Name,
             Input = "{}",
             Triggers =
             {
                 { "redeployment", DateTime.UtcNow.ToString(CultureInfo.InvariantCulture) }
             },
-      });
-   }
+        }, new CustomResourceOptions { DependsOn = { migrationLambda } });
+    }
 }
